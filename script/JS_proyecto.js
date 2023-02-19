6bf7cda8c9e866fee8ac9d5b9f54d0fa89b3173e
Giuliano Cersosimo <giulianocersosimo62@gmail.com>
2023-02-19 15:12:04 -0300

    Primera_Version_del_ProyectoFinal

---
+++ script/JS_proyecto.js
@@ -0,0 +1,10 @@
+// JAVA DE NAVBAR //
+
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0]
+const navmenu = document.getElementsByClassName("nav-menu")[0]
+
+toggleBtn.addEventListener ("click", () => {
+    navmenu.classList.toggle("active")
+})
+
+
